9843b7f8d8e074823f16d084e5ab284b91a2a5c9
mjimenez60 <mjimenez60@mail.pima.edu>
2024-09-26 22:33:51 -0700

    Update cis129_lab02_shortStory.py
    Deleted 'Hello World!' print and also added some comments and a short story

--- cis129_lab02_shortStory.py
+++ cis129_lab02_shortStory.py
@@ -1,1 +1,4 @@
-print('Hello, World!')
+#Author: Miguel Jimenez
+#This is my first Python lab in my CIS129 course
+#I deleted the 'Hello World!" print in order to type comments within this file from within code-edits and also added a short story
+print('My family has had a huge ranch in Mexico for the past 3 generations. It is filled with cows, a few horses, dogs, cats, chickens, and several small brick houses laid across the territory. I like to visit there with my family once a year')
